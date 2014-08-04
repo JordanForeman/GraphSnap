c6f7baf4cc535b7227ac616b16f008c4d6acee10
Jordan Foreman <thejordanforeman@gmail.com>
2014-08-04 14:23:22 -0500

    fix ref

--- config/passport.js
+++ config/passport.js
@@ -4,7 +4,7 @@
 var LocalStrategy   = require('passport-local').Strategy;
 
 // load up the user model
-var User       		= require('../components/user');
+var User       		= require('../components/User');
 
 // expose this function to our app using module.exports
 module.exports = function(passport) {
